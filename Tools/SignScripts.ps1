0444d473d3dd445ad7b5fd4fc7d46f47c5362483
Keith Hill <r_keith_hill@hotmail.com>
2017-09-05 23:03:49 -0600

    Update to newer code-signing cert thumbprint

--- Tools/SignScripts.ps1
+++ Tools/SignScripts.ps1
@@ -1,11 +1,10 @@
-param([string]$RootDir, [string]$CertThumbPrint = '2BE1DA66486C6EE58D5EA3BC692F86EEE413D35C')
+param([string]$RootDir, [string]$CertThumbPrint = 'BB25149CDAF879A29DB6A011F6FC874AF32CBF51')
 
 # This script is invoked by the Pscx project's post build event.
 $cert = Get-ChildItem Cert:\CurrentUser\My\$CertThumbPrint
 
-Get-ChildItem $RootDir -Recurse -Include *.ps1,*.psm1,*.ps1xml -File | 
-    Where Name -ne 'Pscx.UserPreferences.ps1' | 
+Get-ChildItem $RootDir -Recurse -Include *.ps1,*.psm1,*.ps1xml -File |
+    Where Name -ne 'Pscx.UserPreferences.ps1' |
     Foreach {
         Set-AuthenticodeSignature -Certificate $cert -TimestampServer http://timestamp.digicert.com $_.FullName
     }
-     
