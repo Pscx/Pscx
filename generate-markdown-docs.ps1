c8a3d5af33a0e45f6a18d16eb3b1aff31cf763a3
Mike MacCana <mike@certsimple.com>
2017-09-27 16:22:07 +0100

    Comment some of the thinking behind generating docs script

--- generate-markdown-docs.ps1
+++ generate-markdown-docs.ps1
@@ -1,6 +1,9 @@
 $cmdLets = (Get-Module pscx).ExportedCmdlets.Keys
 $commands = (Get-Module pscx).ExportedCommands.Keys
-$cmdLetsAndCommands = $cmdLets + $commands | Select -uniq | Sort-Object | where{ $_ -match "^[A-Z]"}
+# Some cmdlets and commands have the same name
+# Extract only uppercase items as the lowercase versiosn are typically
+# aliases and better documented elsewhere.
+$cmdLetsAndCommands = $cmdLets + $commands | Select -uniq | Sort-Object | where { $_ -cmatch "^[A-Z]"}
 foreach ( $cmdLet in ($cmdLetsAndCommands) ) {
   $description = (get-help $cmdLet).synopsis.replace('PSCX Cmdlet: ','')
   $output = @'
