eab48ea13e8035f2c175bac44153d073c6db94cb
Mike MacCana <mike@certsimple.com>
2017-09-28 11:53:00 +0100

    Remove dead comment.

--- generate-markdown-docs.ps1
+++ generate-markdown-docs.ps1
@@ -1,8 +1,5 @@
 $cmdLets = (Get-Module pscx).ExportedCmdlets.Keys
 $functions = (Get-Module pscx).ExportedFunctions.Keys
-# Some cmdlets and commands have the same name
-# Extract only uppercase items as the lowercase versiosn are typically
-# aliases and better documented elsewhere.
 $cmdLetsAndFunctions = $cmdLets + $functions | Select -uniq | Sort-Object
 foreach ( $cmdLet in ($cmdLetsAndFunctions) ) {
   $description = (get-help $cmdLet).synopsis.replace('PSCX Cmdlet: ','')
